edb44436bbe34af8e0465625151f6bd442b07688
sailist <sailist@outlook.com>
2020-07-12 15:30:25 +0800

    添加了对yield的支持，如果返回迭代器，那么会消耗掉迭代器并返回最后一次迭代的结果

--- thexp/frame/trainer.py
+++ thexp/frame/trainer.py
@@ -1,21 +1,5 @@
 """
-    Copyright (C) 2020 Shandong University
 
-    This program is licensed under the GNU General Public License 3.0 
-    (https://www.gnu.org/licenses/gpl-3.0.html). 
-    Any derivative work obtained under this license must be licensed 
-    under the GNU General Public License as published by the Free 
-    Software Foundation, either Version 3 of the License, or (at your option) 
-    any later version, if this derivative work is distributed to a third party.
-
-    The copyright for the program is owned by Shandong University. 
-    For commercial projects that require the ability to distribute 
-    the code of this program as part of a program that cannot be 
-    distributed under the GNU General Public License, please contact 
-            
-            sailist@outlook.com
-             
-    to purchase a commercial license.
 """
 import bisect
 import os
@@ -29,7 +13,7 @@ from functools import lru_cache
 from functools import wraps
 from torch.utils.data.dataloader import DataLoader
 from typing import Any, Union, List, Dict
-
+from collections.abc import Iterator
 from .databundler import DataBundler
 from .meter import AvgMeter, Meter
 from .params import Params
@@ -69,6 +53,12 @@ class BaseTrainer(metaclass=Merge):
                     callback.on_begin(self, func, self.params, *args, **kwargs)
                 try:
                     _meter = func(*args, **kwargs)
+                    # 如果返回迭代器，那么会消耗掉迭代器，并只返回最后一次运行的结果（如果有）
+                    if isinstance(_meter, Iterator):
+                        _m = Meter()
+                        for _m in _meter:
+                            pass
+                        _meter = _m
                 except BaseException as e:
                     _handles = [callback.on_exception(self, func, self.params, e, *args, **kwargs)
                                 for callback in _call_set]
