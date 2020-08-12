0844eebd6c363c83a9e728085de9525ff17a61d3
sailist <sailist@outlook.com>
2020-08-12 17:48:04 +0800

    fix tensorboard kwargs

--- thexp/cli/cli.py
+++ thexp/cli/cli.py
@@ -56,9 +56,12 @@ def _board_with_logdir(logdir, *args, **kwargs):
 
     tmpdir = os.path.join(os.path.dirname(logdir), 'board_tmp')
     os.makedirs(tmpdir, exist_ok=True)
-
-    subprocess.check_call(['tensorboard', '--logdir={}'.format(logdir)],
-                          env=dict(os.environ, TMPDIR=tmpdir))
+    try:
+        subprocess.check_call(['tensorboard', '--logdir={}'.format(logdir), *['--{}={}' for k, v in kwargs.items()]],
+                              env=dict(os.environ, TMPDIR=tmpdir))
+    except KeyboardInterrupt as k:
+        print('bye.')
+        exit(0)
 
 
 def _board_with_test_name(test_name, *args, **kwargs):
