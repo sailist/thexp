715222a44db882ca2851797c6ee1878ff5f76c5e
sailist <sailist@outlook.com>
2020-09-17 19:14:34 +0800

    fix some bugs

--- thexp/frame/databundler.py
+++ thexp/frame/databundler.py
@@ -35,17 +35,39 @@ class DataBundler:
         self.dataloaders = DataBundler.DataBundler()
         self.iter_mode = "chain"
         self._to_device_fn = to_device_fn
+        self.device_arg = None
         if device is not None:
             self.to(device)
 
-    def set_batch_size(self, batch_size):
-        from torch.utils.data import DataLoader
-        from thexp.contrib.data.dataloader import DataLoader as thDataLoader
-        for _, (loader, _) in self.dataloaders.items():
-            if isinstance(loader, thDataLoader):
-                loader.set_batch_size(batch_size)
-            elif isinstance(loader, DataLoader):
-                loader.batch_sampler.batch_size = batch_size
+    def __len__(self):
+        if self.iter_mode == 'zip':
+            return min(len(loader) for name, (loader, func) in self.dataloaders.items() if func.__name__ != 'cycle')
+        elif self.iter_mode == "chain":
+            return sum(self.len_list())
+
+    def __getitem__(self, item):
+        return self.dataloaders[item][0]
+
+    def __iter__(self):
+        loaders = self._func_loader()
+        if len(loaders) == 1:
+            iter = loaders[0]
+            # for batch in loaders[0]:
+            #     yield batch
+            # return iter(loaders[0])
+        elif self.iter_mode == "zip":
+            iter = zip(*loaders)
+        elif self.iter_mode == "chain":
+            iter = chain(*loaders)
+        else:
+            assert False
+
+        for batch_data in iter:
+            # from .utils import to_device
+            if self.device_arg is not None:
+                yield self._to_device_fn(batch_data, self.device_arg)
+            else:
+                yield batch_data
 
     def _append(self, loader, func, name):
         from torch.utils.data import DataLoader
@@ -61,12 +83,25 @@ class DataBundler:
             assert name not in self.dataloaders, "{} also defined in bundler".format(name)
 
         self.dataloaders[name] = (loader, func)
+        if isinstance(loader, DataBundler) and self.device_arg is not None:
+            loader.to(*self.device_arg[0], **self.device_arg[1])
 
-    def __len__(self):
-        if self.iter_mode == 'zip':
-            return min(len(loader) for name, (loader, func) in self.dataloaders.items() if func.__name__ != 'cycle')
-        elif self.iter_mode == "chain":
-            return sum(self.len_list())
+    def _func_loader(self):
+        def filter(loader):
+            if isinstance(loader, DataBundler):
+                return iter(loader)
+            return loader
+
+        return [func(filter(loader)) for name, (loader, func) in self.dataloaders.items()]
+
+    def set_batch_size(self, batch_size):
+        from torch.utils.data import DataLoader
+        from thexp.contrib.data.dataloader import DataLoader as thDataLoader
+        for _, (loader, _) in self.dataloaders.items():
+            if isinstance(loader, thDataLoader):
+                loader.set_batch_size(batch_size)
+            elif isinstance(loader, DataLoader):
+                loader.batch_sampler.batch_size = batch_size
 
     def len_list(self):
         """
@@ -100,38 +135,6 @@ class DataBundler:
         self._append(loader, lambda x: x, name)
         return self
 
-    def _func_loader(self):
-        def filter(loader):
-            if isinstance(loader, DataBundler):
-                return iter(loader)
-            return loader
-
-        return [func(filter(loader)) for name, (loader, func) in self.dataloaders.items()]
-
-    def __getitem__(self, item):
-        return self.dataloaders[item][0]
-
-    def __iter__(self):
-        loaders = self._func_loader()
-        if len(loaders) == 1:
-            iter = loaders[0]
-            # for batch in loaders[0]:
-            #     yield batch
-            # return iter(loaders[0])
-        elif self.iter_mode == "zip":
-            iter = zip(*loaders)
-        elif self.iter_mode == "chain":
-            iter = chain(*loaders)
-        else:
-            assert False
-
-        for batch_data in iter:
-            # from .utils import to_device
-            if self.device_arg is not None:
-                yield self._to_device_fn(batch_data, self.device_arg)
-            else:
-                yield batch_data
-
     @overload
     def to(self, dtype: torch.dtype, non_blocking: bool = False, copy: bool = False):
         ...
@@ -153,6 +156,9 @@ class DataBundler:
             device = args[0]
         assert isinstance(device, (torch.device, str))
         self.device_arg = [args, kwargs]
+        for (loader, _) in self.dataloaders.values():
+            if isinstance(loader, DataBundler):
+                loader.to(*args, **kwargs)
         return self
 
     def choice_batch(self) -> tuple:
