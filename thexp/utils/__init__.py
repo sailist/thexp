9124fce9e05e17cfbcad59d19cc7da2da8424034
sailist <sailist@outlook.com>
2020-12-28 11:04:14 +0800

    优化了 re 的来源

--- thexp/utils/__init__.py
+++ thexp/utils/__init__.py
@@ -2,9 +2,15 @@
 
 """
 
+try:
+    import regex as re
+except:
+    import re
+
+
 
 from .paths import home_dir
 from .timing import timeit
 from .screen import ScreenStr
-home_dir()
 
+home_dir()
