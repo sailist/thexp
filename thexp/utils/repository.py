f22eb9471de2c9f2f14e109d741a20612888957c
sailist <sailist@outlook.com>
2020-12-28 16:13:59 +0800

    update gitignore for thexp template

--- thexp/utils/repository.py
+++ thexp/utils/repository.py
@@ -15,10 +15,13 @@ from .dates import curent_date
 from ..globals import _GITKEY, _OS_ENV, _FNAME
 
 torch_file = ['*.pth', '*.npy', '*.ckpt']
-thexp_gitignores = ['.thexp/', _FNAME.repo, _FNAME.expsdirs, '.idea/', '*.thexp.*'] + torch_file
+thexp_gitignores = ['.thexp/', _FNAME.repo, _FNAME.expsdirs, '.idea/', '*.thexp.*', '*.pkl'] + torch_file
 
-py_gitignore = "\n".join(['# Byte-compiled / optimized / DLL files', '__pycache__/', '*.py[cod]',
-                          '*$py.class', '', '# C extensions', '*.so', '', '# Distribution / packaging',
+py_gitignore = "\n".join(['# Byte-compiled / optimized / DLL files',
+                          '__pycache__/', '*.py[cod]',
+                          '*$py.class',
+                          '# C extensions', '*.so',
+                          '# Distribution / packaging',
                           '.Python', 'build/', 'develop-eggs/', 'dist/', 'downloads/', 'eggs/', '.eggs/',
                           'lib/', 'lib64/', 'parts/', 'sdist/', 'var/', 'wheels/', 'pip-wheel-metadata/',
                           'share/python-wheels/', '*.egg-info/', '.installed.cfg', '*.egg', 'MANIFEST',
@@ -26,22 +29,31 @@ py_gitignore = "\n".join(['# Byte-compiled / optimized / DLL files', '__pycache_
                           '#  Usually these files are written by a python script from a template',
                           '#  before PyInstaller builds the exe, so as to inject date/other infos into '
                           'it.',
-                          '*.manifest', '*.spec', '', '# Installer logs', 'pip-log.txt',
-                          'pip-delete-this-directory.txt', '', '# Unit test / coverage reports',
+                          '*.manifest', '*.spec',
+                          '# Installer logs', 'pip-log.txt',
+                          'pip-delete-this-directory.txt',
+                          '# Unit test / coverage reports',
                           'htmlcov/', '.tox/', '.nox/', '.coverage', '.coverage.*', '.cache',
                           'nosetests.xml', 'coverage.xml', '*.cover', '.hypothesis/', '.pytest_cache/',
-                          '', '# Translations', '*.mo', '*.pot', '', '# Django stuff:', '*.log',
-                          'local_settings.py', 'db.sqlite3', '', '# Flask stuff:', 'instance/',
-                          '.webassets-cache', '', '# Scrapy stuff:', '.scrapy', '',
+                          '# Translations', '*.mo', '*.pot',
+                          '# Django stuff:', '*.log', 'local_settings.py', 'db.sqlite3',
+                          '# Flask stuff:',
+                          'instance/', '.webassets-cache', '', '# Scrapy stuff:', '.scrapy', '',
                           '# Sphinx documentation', 'docs/_build/', '', '# PyBuilder', 'target/', '',
-                          '# Jupyter Notebook', '.ipynb_checkpoints', '', '# IPython',
-                          'profile_default/', 'ipython_config.py', '', '# pyenv', '.python-version', '',
+                          '# Jupyter Notebook', '.ipynb_checkpoints', '',
+                          '# IPython',
+                          'profile_default/', 'ipython_config.py',
+                          '# pyenv', '.python-version', '',
                           '# celery beat schedule file', 'celerybeat-schedule', '',
-                          '# SageMath parsed files', '*.sage.py', '', '# Environments', '.env', '.venv',
+                          '# SageMath parsed files', '*.sage.py', '',
+                          '# Environments', '.env', '.venv',
                           'env/', 'venv/', 'ENV/', 'env.bak/', 'venv.bak/', '',
                           '# Spyder project settings', '.spyderproject', '.spyproject', '',
-                          '# Rope project settings', '.ropeproject', '', '# mkdocs documentation',
-                          '/site', '', '# mypy', '.mypy_cache/', '.dmypy.json', 'dmypy.json', '',
+                          '# Rope project settings', '.ropeproject', '',
+                          '# mkdocs documentation',
+                          '/site', '',
+                          '# mypy',
+                          '.mypy_cache/', '.dmypy.json', 'dmypy.json',
                           '# Pyre type checker', '.pyre/'] + thexp_gitignores)
 
 
